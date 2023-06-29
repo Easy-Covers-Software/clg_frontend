06d63dc19806049788e03a7ee1737785a5166f95
David Silveira <daviewave@gmail.com>
2023-06-29 19:37:22 -0400

    added login dialog to the Home page and return when sign in button is clicked

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+'use client';
+
 import * as React from 'react';
 
 export interface IHomeProps {}
 
+import { useLoginContext } from '@/context/StateContext';
+
+import LoginDialog from '@/components/Login/LoginDialog';
+
 export default function Home(props: IHomeProps) {
-  return <div></div>;
+  const { isLoginOpen, toggleLoginIsOpen } = useLoginContext();
+
+  if (isLoginOpen) {
+    return <LoginDialog />;
+  }
+
+  return <></>;
 }
