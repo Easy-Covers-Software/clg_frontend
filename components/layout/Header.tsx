3704ee7d0c02a14de88fc3c27557fbd7396ded94
David Silveira <daviewave@gmail.com>
2023-06-29 00:50:04 -0400

    removed all unneeded files and moved around some files

--- components/layout/Header.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export interface IHeaderProps {}
-
-export default function Header(props: IHeaderProps) {
-  return <div className="header"></div>;
-}
--- components/layout/Sidebar.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export interface ISidebarProps {}
-
-export default function Sidebar(props: ISidebarProps) {
-  return <></>;
-}
