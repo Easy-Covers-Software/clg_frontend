a95cafc633afcac3583716642b40033c6cc96f41
David Silveira <daviewave@gmail.com>
2023-06-30 02:32:32 -0400

    decreased border radius on buttons

--- components/Global.tsx
+++ components/Global.tsx
@@ -9,7 +9,7 @@ const PrimaryButton = styled(Button)`
   text-transform: uppercase;
   letter-spacing: 2px;
   cursor: pointer;
-  border-radius: 10px;
+  border-radius: 4px;
   border: 2px solid #00bfa6;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
     rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
@@ -37,7 +37,7 @@ const UnSelectedButton = styled(Button)`
   text-transform: uppercase;
   letter-spacing: 2px;
   cursor: pointer;
-  border-radius: 10px;
+  border-radius: 4px;
   border: 2px solid #00bfa6;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
     rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
