c7b94b33436dd8a5aa544782336750fde7492928
David Silveira <daviewave@gmail.com>
2023-06-30 15:35:01 -0400

    added generation setup componenet -- just the skeleton for now -- + job posting input

---
+++ components/GenerationSetup/GenerationSetup.tsx
@@ -0,0 +1,135 @@
+import * as React from 'react';
+// import { styled } from '@mui/material/styles';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
+import MuiAccordionSummary, {
+  AccordionSummaryProps,
+} from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+
+import Grid from '@mui/material/Unstable_Grid2/Grid2';
+import styled from '@emotion/styled';
+
+import JobPostingInput from './components/JobPostingInput';
+
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(() => ({
+  backgroundColor: '#fff',
+  border: `1px solid black`,
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&:before': {
+    display: 'none',
+  },
+}));
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+  />
+))(() => ({
+  backgroundColor: 'rgba(255, 255, 255, .05)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: '10px',
+  },
+}));
+const AccordionDetails = styled(MuiAccordionDetails)(() => ({
+  padding: 0,
+  margin: 0,
+  borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
+
+// Want to eventually change this depending on if a generation has already occured or not
+const Container = styled(Grid)`
+  width: 45%;
+  padding: 0.8%;
+  background-color: #f8f8ff;
+  border-radius: 4px;
+  border: 1px solid #006d4b;
+  height: 100%;
+`;
+
+export default function GenerationSetup() {
+  const [expanded, setExpanded] = React.useState<string | false>('panel1');
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      if (isExpanded || expanded !== panel) {
+        setExpanded(panel);
+      }
+    };
+
+  return (
+    <Container>
+      {/* Job Posting */}
+      <Accordion
+        expanded={expanded === 'panel1'}
+        disableGutters
+        onChange={handleChange('panel1')}
+        style={{
+          height: 'auto',
+        }}
+      >
+        {/* ACCORDION LABEL */}
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography>Job Posting</Typography>
+        </AccordionSummary>
+
+        {/* ACCORDION CONTENT */}
+        <AccordionDetails>
+          <JobPostingInput />
+        </AccordionDetails>
+      </Accordion>
+
+      {/* Resume Upload */}
+      <Accordion
+        expanded={expanded === 'panel2'}
+        onChange={handleChange('panel2')}
+      >
+        {/* ACCORDION LABEL */}
+        <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+          <Typography>Résumé Upload / Personal Details</Typography>
+        </AccordionSummary>
+
+        {/* ACCORDION CONTENT */}
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      {/* Additional Details */}
+      <Accordion
+        expanded={expanded === 'panel3'}
+        onChange={handleChange('panel3')}
+      >
+        {/* ACCORDION LABEL */}
+        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+          <Typography>Additional Details</Typography>
+        </AccordionSummary>
+
+        {/* ACCORDION CONTENT */}
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Container>
+  );
+}
---
+++ components/GenerationSetup/components/JobPostingInput.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+
+const JobPostingTextInputField = styled.textarea`
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  min-height: 68.4vh;
+  background-color: #f8f8ff;
+  padding: 2%;
+  min-width: 100%;
+  max-width: 100%;
+  max-height: 68.4vh;
+  border: none;
+  ::placeholder {
+    color: #e2e2e2; // Change this to the color you want
+  }
+`;
+
+export default function JobPostingInput() {
+  const [value, setValue] = useState('');
+  const [placeholder, setPlaceholder] = useState(
+    'Either directly copy and paste the job posting you are applying for or provide your own description of the postion you are applying for...',
+  );
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleFocus = () => {
+    setPlaceholder('');
+  };
+
+  const handleBlur = () => {
+    if (value === '') {
+      setPlaceholder(
+        'Either directly copy and paste the job posting you are applying for or provide your own description of the postion you are applying for...',
+      );
+    }
+  };
+
+  return (
+    <JobPostingTextInputField
+      placeholder={placeholder}
+      value={value}
+      onChange={handleChange}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+    />
+  );
+}
