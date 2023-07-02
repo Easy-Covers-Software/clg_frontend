9a6d4bb1329dd354b88b35b02c7323012ad35a03
David Silveira <daviewave@gmail.com>
2023-07-02 03:00:53 -0400

    added medium and custom re query components

---
+++ components/Results/components/ReQueryOptions/components/CustomReQuery.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Unstable_Grid2/Grid2';
+
+const CustomReQueryField = styled.textarea`
+  width: 100%;
+  height: 100%;
+  resize: none;
+  box-sizing: border-box;
+  background-color: #fff;
+  padding: 2%;
+  min-width: 100%;
+  max-width: 100%;
+  border-top: 1px solid #006d4b;
+  border-radius: 4px;
+  ::placeholder {
+    color: #e2e2e2; // Change this to the color you want
+  }
+`;
+
+export default function CustomReQuery() {
+  const [value, setValue] = useState('');
+  const [placeholder, setPlaceholder] = useState(
+    'Add any information that is not in your resume that you think is relevant to your application',
+  );
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleFocus = () => {
+    setPlaceholder('');
+  };
+
+  const handleBlur = () => {
+    if (value === '') {
+      setPlaceholder(
+        'Either directly copy and paste the job posting you are applying for or provide your own description of the postion you are applying for...',
+      );
+    }
+  };
+
+  return (
+    <Grid
+      display={'flex'}
+      flexDirection={'column'}
+      // justifyContent={'space-between'}
+      height={'100%'}
+      width={'14vw'}
+      gap={2}
+      mt={2}
+    >
+      <Typography ml={1} mb={-2} fontSize={'0.8rem'}>
+        Custom Adjustment
+      </Typography>
+      <CustomReQueryField
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
+    </Grid>
+  );
+}
---
+++ components/Results/components/ReQueryOptions/components/MediumReQueryInput.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import Grid from '@mui/material/Unstable_Grid2/Grid2';
+import Typography from '@mui/material/Typography';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import Tooltip from '@mui/material/Tooltip';
+
+import styled from '@emotion/styled';
+
+const Container = styled(Grid)`
+  display: flex;
+  align-items: center;
+  width: 14vw;
+  gap: 16px;
+`;
+
+const FormInput = styled(FormControl)`
+  width: 100%;
+  padding-right: 0;
+`;
+
+const InputField = styled(TextField)`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  border-radius: 4px;
+  background-color: #fff;
+  // background-color: #f8f8ff;
+`;
+
+const QuestionContainer = styled(Grid)`
+  display: flex;
+  justify-content: space-between;
+  margin: 0 5px;
+`;
+
+export default function MediumReQueryInput({ label }) {
+  const [value, setValue] = React.useState('');
+  const [value2, setValue2] = React.useState('');
+
+  const clearInput = () => {
+    setValue('');
+  };
+
+  return (
+    <Container>
+      <FormInput variant="outlined">
+        <QuestionContainer>
+          <Typography fontSize={'0.7rem'} mt={0.5}>
+            {label}
+          </Typography>
+
+          <Tooltip title="Delete" placement="top">
+            <InfoOutlinedIcon fontSize="small" sx={{ opacity: '40%' }} />
+          </Tooltip>
+        </QuestionContainer>
+
+        <InputField
+          id="email-input"
+          variant="outlined"
+          placeholder="2021-present"
+          size="small"
+          value={value}
+          // sx={{ opacity: '30%' }}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setValue(event.target.value);
+          }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={clearInput}
+                  edge="end"
+                  sx={{ opacity: '30%' }}
+                >
+                  <HighlightOffIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </FormInput>
+    </Container>
+  );
+}
