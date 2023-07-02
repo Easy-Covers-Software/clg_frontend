8a49c058931756195919e91b0dedda529d0cfbf7
David Silveira <daviewave@gmail.com>
2023-07-02 03:03:27 -0400

    created context for the more options button

--- components/Results/Results.tsx
+++ components/Results/Results.tsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
 import styled from '@emotion/styled';
 
+import ResultsSummary from './components/ResultsSummary';
+import CoverLetterResults from './components/CoverLetterResults/CoverLetterResults';
+import ReQueryOptions from './components/ReQueryOptions/ReQueryOptions';
+
 // Want to eventually change this depending on if a generation has already occured or not
 const Container = styled(Grid)`
+  // width: ${({ isReQuerySectionExpanded }) =>
+    isReQuerySectionExpanded === 'upload' ? 'auto' : '100%'};
   width: 56%;
-  padding: 1%;
   background-color: #f8f8ff;
   border-radius: 4px;
   border: 1px solid #006d4b;
   height: calc(100vh - 100px);
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
 `;
 
+export const ReQueryContext = React.createContext(null);
+
 export default function Results() {
-  return <Container></Container>;
+  const [isReQuerySectionExpanded, setIsReQuerySectionExpanded] =
+    useState(false);
+
+  const toggleIsReQuerySectionExpanded = () => {
+    setIsReQuerySectionExpanded(!isReQuerySectionExpanded);
+  };
+
+  console.log('isReQuerySectionExpanded:', isReQuerySectionExpanded);
+  return (
+    <ReQueryContext.Provider
+      value={{ isReQuerySectionExpanded, toggleIsReQuerySectionExpanded }}
+    >
+      <Container>
+        <ResultsSummary />
+        <CoverLetterResults />
+        <ReQueryOptions />
+      </Container>
+    </ReQueryContext.Provider>
+  );
 }
