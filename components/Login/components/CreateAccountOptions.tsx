81bf6f39e527f41e6653978668f058339a4bcb6d
David Silveira <daviewave@gmail.com>
2023-07-04 21:34:34 -0400

    call the sign in with Google when the relevant button is clicked

--- components/Login/components/CreateAccountOptions.tsx
+++ components/Login/components/CreateAccountOptions.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 import React from 'react';
 
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
 import styled from '@emotion/styled';
 
 import { IconButton } from '@mui/material';
+
+import { useSession } from 'next-auth/react';
+import { signIn } from 'next-auth/react';
 
 const Container = styled(Grid)`
   display: flex;
@@ -15,43 +20,37 @@ const IconContainer = styled(Grid)`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 6vw;
-  height: 3vw;
+  width: 10vw;
+  height: 4vw;
   border-radius: 8px;
   background-color: #fff;
   border: 1px solid #006d4b;
+  cursor: pointer;
 `;
 
 export default function AuthProviderButtons() {
-  const handleGoogleAuth = () => {
-    // Implement Google Authentication here
-  };
+  const { data: session, status } = useSession();
 
-  const handleAppleAuth = () => {
-    // Implement Apple Authentication here
-  };
-
-  const handleFacebookAuth = () => {
-    // Implement Facebook Authentication here
+  const createAccount = () => {
+    // need to create an endpoint in the backend to create a user
   };
 
   return (
     <Container>
-      <IconContainer>
-        <IconButton onClick={handleGoogleAuth}>
-          <img src="/GoogleIcon.svg" alt="google icon" />
+      <IconContainer onClick={() => signIn()}>
+        <IconButton disableFocusRipple disableRipple>
+          <img src="/GoogleIcon.svg" alt="google icon" width={40} height={40} />
         </IconButton>
       </IconContainer>
 
       <IconContainer>
-        <IconButton onClick={handleAppleAuth}>
-          <img src="/AppleIcon.svg" alt="google icon" />
-        </IconButton>
-      </IconContainer>
-
-      <IconContainer>
-        <IconButton onClick={handleFacebookAuth}>
-          <img src="/FacebookIcon.svg" alt="google icon" />
+        <IconButton onClick={createAccount} disableRipple disableFocusRipple>
+          <img
+            src="/easy-covers-full.svg"
+            alt="google icon"
+            width={130}
+            height={130}
+          />
         </IconButton>
       </IconContainer>
     </Container>
