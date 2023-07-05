2fc2775193edef4df3a8ed2ca3e34e1ff15cfaf2
David Silveira <daviewave@gmail.com>
2023-07-04 21:31:55 -0400

    retrieved the session from server side props .. not sure if this is correct or not but works so whatever

---
+++ components/Provider.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { FC, ReactNode } from 'react';
+import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
+
+import { getSession } from 'next-auth/react';
+
+type Props = {
+  children: ReactNode;
+  session: Session | null | undefined;
+};
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: { session },
+  };
+}
+
+const Provider: FC<Props> = ({ children, session }) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+};
+
+export default Provider;
