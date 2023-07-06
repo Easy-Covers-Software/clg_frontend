1c063d10573b3c4a72db61e5ca23477809edfd5f
David Silveira <daviewave@gmail.com>
2023-07-06 01:19:02 -0400

    finally - finally - got the jwt token to correctly send

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,31 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import LinkedInProvider from 'next-auth/providers/linkedin';
-
 import axios from 'axios';
+
+import { isJwtExpired, makeUrl } from '@/client/constants/Utils';
+
+export const refreshToken = async (refreshToken) => {
+  try{
+    const response = await axios.post(
+      'http://localhost:8000/api/token/refresh/',
+      // makeUrl(
+      //   process.env.BACKEND_BASE_URL,
+      //   'token',
+      //   'refresh',
+      // ),
+      {
+        refresh: refreshToken,
+      },
+    );
+
+    const { access, refresh } = response.data;
+    return [access, refresh]
+  } catch (error) {
+    return [null, null]
+  }
+}
+
+
 
 export const authOptions = {
   providers: [
@@ -12,60 +35,107 @@ export const authOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    jwt: true,
+    maxAge: 24 * 60 * 60, // 24 hours
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET_KEY,
+  },
+  debug: process.env.NODE_ENV === 'development',
 
+  // FLOW: jwt -> session
+  // 1. user logs in with google and the login endpoint
+  // 2. the jwt function is called, which is used to add the access_token to the jwt token
+  // 3. the session function is called, which is used to add the jwt token to the session
+  //* Each time the session is called in useSession(), getSession(), the jwt() and session callbacks are run again
+  //* As such, the refreshing mechansism needs tobe done in the jwt() callback
   callbacks: {
-    async signIn({ user, account, profile }) {
-      const { id_token } = account;
-
-      try {
-        const response = await axios.post(
-          'http://localhost:8000/api/social/login/google/',
-          {
-            access_token: id_token,
-          },
-        );
-
-        const { access_token } = response.data;
-        user.access_token = access_token;
-        return true;
-      } catch (error) {
-        // console.log('Error exchanging tokens: ', error);
-        return false;
-      }
-    },
-
     async jwt({ token, user, account, profile, isNewUser }) {
       if (user) {
-        const { access_token } = user;
-        token.access_token = access_token;
+        const { id_token } = account;
+        try {
+          const response = await axios.post(
+            "http://127.0.0.1:8000/api/google/",
+            // makeUrl(
+            //   process.env.BACKEND_API_BASE,
+            //   "social",
+            //   "login",
+            //   account.provider,
+            // ),
+            {
+              access_token: id_token,
+              id_token: id_token
+            },
+          );
+
+          // extract the returned token from the DRF backend and add it to the `user` object          
+          const { user, tokens } = response.data;
+          const { access_token, refresh_token } = tokens
+          
+          token = {
+            ...token,
+            access_token: access_token,
+            refresh_token: refresh_token,
+          };
+
+
+          return token;
+        } catch (error) {
+          console.log('error: user not found');
+          // console.log(error)
+          return false;
+
+        }
       }
 
+      // user was signed in previously, we want to check if the token needs refreshing
+      // token has been invalidated, try refreshing it
+      if(isJwtExpired(token.access_token)){
+        const [
+          newAccessToken,
+          newRefreshToken,
+        ] = await refreshToken(token.refresh_token);
+
+        if(newAccessToken && newRefreshToken) {
+          token = {
+            ...token,
+            access_token: newAccessToken,
+            refresh_token: newRefreshToken,
+            iat: Math.floor(Date.now() / 1000),
+            exp: Math.floor(Date.now() / 1000 + 2 * 60 * 60) 
+          };
+          return token;
+        }
+
+        // unable to refresh tokens from DRF backend, invalidate the token
+        return {
+          ...token,
+          exp: 0,
+        }
+      }
       return token;
     },
 
     async session({ session, token }) {
-      console.log('session --*: ', session);
-      console.log('token --*: ', token);
+      console.log('session token: ', token)
+      const { name, email, picture, access_token, refresh_token } = token;
+      
+      const tokens = {
+        access_token,
+        refresh_token,
+      }
 
-      // try {
-      //   const response = await axios.get(
-      //     'http://localhost:8000/api/social/login/users/',
-      //   );
+      const user = {
+        name,
+        email,
+        picture,
+        tokens
+      }
 
-      //   console.log('response: ');
-      //   // console.log(response);
+      session.user = user;
+      session.tokens = tokens;
 
-      //   return true;
-      // } catch (error) {
-      //   console.log('Error Fetching Users: ', error);
-      //   return false;
-      // }
-
-      // session.user.name = token.token.name;
-      // session.user.email = token.token.email;
-      // session.user.image = token.token.picture;
-
-      session.access_token = token.access_token;
       return session;
     },
   },
