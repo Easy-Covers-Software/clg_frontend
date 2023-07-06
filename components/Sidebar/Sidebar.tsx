4f46835e672859825b1f2f476ece5f2b3cc2e3ee
David Silveira <daviewave@gmail.com>
2023-07-06 03:06:57 -0400

    added conditional rendering of menu depending on who is logged in

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 import MenuLoggedOut from './components/MenuLoggedOut';
+import MenuLoggedIn from './components/MenuLoggedIn/MenuLoggedIn';
+import { useSession } from 'next-auth/react';
 
 const Logo = styled.img`
   margin-top: -55%;
 `;
 
+type UserInfo = {
+  name: string;
+  email: string;
+  picture: string;
+}
+
 export default function Sidebar() {
+  const { data: session } = useSession()
+  const user = session?.user
+  const userInfo: UserInfo = user?.info
+
+  console.log('user', userInfo)
+
   return (
     <>
       <Logo
@@ -16,7 +30,11 @@ export default function Sidebar() {
         width={'100%'}
       />
 
-      <MenuLoggedOut />
+      {user ? (
+        <MenuLoggedIn userInfo={userInfo} />
+      ) : (
+        <MenuLoggedOut />
+      )}
     </>
   );
 }
