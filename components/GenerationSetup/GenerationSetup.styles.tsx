dad8e4837efd17b5048f3f1d024622a16dd73f5d
David Silveira <daviewave@gmail.com>
2023-07-21 02:51:59 -0400

    cleaned up Generation setup and added seperate file for styled components

---
+++ components/GenerationSetup/GenerationSetup.styles.tsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import { UnSelectedButton } from "../Global";
+
+// Material UI related imports
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
+import MuiAccordionSummary, {
+  AccordionSummaryProps,
+} from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import KeyboardDoubleArrowRightOutlinedIcon from "@mui/icons-material/KeyboardDoubleArrowRightOutlined";
+import KeyboardDoubleArrowLeftOutlinedIcon from "@mui/icons-material/KeyboardDoubleArrowLeftOutlined";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+
+// Accordion styles
+const Accordion = styled((props: AccordionProps & { currPanel?: string }) => {
+  const { currPanel, ...otherProps } = props;
+  return <MuiAccordion disableGutters elevation={0} square {...otherProps} />;
+})(({ currPanel }) => ({
+  backgroundColor: "#f8f8ff",
+  border: "1px solid #006D4B",
+  borderBottom: currPanel === "panel3" ? "1px solid #006D4B" : "none",
+  borderRadius:
+    currPanel === "panel1"
+      ? "4px 4px 0 0"
+      : currPanel === "panel3"
+      ? "0 0 4px 4px"
+      : "none",
+}));
+
+// AccordionSummary styles
+const AccordionSummary = styled(
+  (
+    props: AccordionSummaryProps & {
+      isExpanded?: boolean;
+      expanded?: string;
+      tracker?: string;
+    }
+  ) => {
+    const { isExpanded, expanded, tracker, ...otherProps } = props;
+    return (
+      <MuiAccordionSummary
+        expandIcon={
+          isExpanded && expanded !== "panel3" ? (
+            <KeyboardDoubleArrowRightOutlinedIcon sx={{ fontSize: "0.9rem" }} />
+          ) : (
+            <KeyboardDoubleArrowLeftOutlinedIcon sx={{ fontSize: "0.9rem" }} />
+          )
+        }
+        {...otherProps}
+      />
+    );
+  }
+)(() => ({
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+  },
+  "& .MuiAccordionSummary-content": { display: "flex", gap: "1.5%" },
+}));
+
+// AccordionDetails styles
+const AccordionDetails = styled(MuiAccordionDetails)(() => ({
+  padding: "0.2%",
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+  height: "calc(100vh - 320px)",
+  backgroundColor: "white",
+  overflow: "auto",
+}));
+
+// Container styles
+const Container = styled(Grid)`
+  width: 46%;
+  padding: 0.3%;
+  background-color: white;
+  border-radius: 4px;
+  border: 1px solid #006d4b;
+  height: calc(100vh - 100px);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+// SubContainer styles
+const SubContainer = styled(Grid)`
+  width: 100%;
+`;
+
+// // GenerateButton styles
+const GenerateButton = styled(UnSelectedButton)`
+  width: 55%;
+  background-color: #bacbba;
+  color: white;
+  font-size: 0.95rem;
+  letter-spacing: 1px;
+
+  &:hover {
+    background-color: #a5b4a5;
+    color: white;
+  }
+`;
+
+const CheckboxIconInComplete = styled(CheckCircleOutlineIcon)`
+  color: #e9e9e9;
+  opacity: 50%;
+`;
+const CheckboxIconComplete = styled(CheckCircleOutlineIcon)`
+  color: limegreen;
+  opacity: 1;
+`;
+
+const TypographyColored = styled(Typography)`
+  color: #006d4b;
+`;
+
+export {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Container,
+  SubContainer,
+  GenerateButton,
+  CheckboxIconInComplete,
+  CheckboxIconComplete,
+  TypographyColored,
+};
--- components/GenerationSetup/GenerationSetup.tsx
+++ components/GenerationSetup/GenerationSetup.tsx
@@ -1,213 +1,93 @@
 "use client";
 
+// React-related imports
 import React, { useState, useEffect } from "react";
 
-import { styled } from "@mui/material/styles";
-import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Container,
+  SubContainer,
+  GenerateButton,
+  CheckboxIconInComplete,
+  CheckboxIconComplete,
+  TypographyColored,
+} from "./GenerationSetup.styles";
 
-import MuiAccordionDetails from "@mui/material/AccordionDetails";
-import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
-import MuiAccordionSummary, {
-  AccordionSummaryProps,
-} from "@mui/material/AccordionSummary";
-import Typography from "@mui/material/Typography";
-
+// Custom Component Imports
 import JobPostingInput from "./components/JobPostingInput";
 import PersonalDetails from "./components/PersonalDetails/PersonalDetails";
 import AdditionalDetails from "./components/AdditionalDetails/AdditionalDetails";
-import KeyboardDoubleArrowRightOutlinedIcon from "@mui/icons-material/KeyboardDoubleArrowRightOutlined";
-import KeyboardDoubleArrowLeftOutlinedIcon from "@mui/icons-material/KeyboardDoubleArrowLeftOutlined";
-import { PrimaryButton, UnSelectedButton } from "../Global";
 
-import { uploadJobPosting, uploadResume } from "./api/generationSetupEndpoints";
+// Material UI related imports
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import Typography from "@mui/material/Typography";
 
+// Context Imports
 import { useGenerationSetupContext } from "@/context/GenerationSetupContext";
-
 import { useCoverLetterResultsContext } from "@/context/ResultsContext";
 
-import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
-
-const Accordion = styled((props: AccordionProps & { currPanel?: string }) => {
-  const { currPanel, ...otherProps } = props;
-  return <MuiAccordion disableGutters elevation={0} square {...otherProps} />;
-})(({ currPanel }) => ({
-  // backgroundColor: "#fff",
-  backgroundColor: "#f8f8ff",
-  border: "1px solid #006D4B",
-  borderBottom: currPanel === "panel3" ? "1px solid #006D4B" : "none",
-  borderRadius:
-    currPanel === "panel1"
-      ? "4px 4px 0 0"
-      : currPanel === "panel3"
-      ? "0 0 4px 4px"
-      : "none",
-}));
-
-const AccordionSummary = styled(
-  (
-    props: AccordionSummaryProps & {
-      isExpanded?: boolean;
-      expanded?: string;
-      tracker?: string;
-    }
-  ) => {
-    const { isExpanded, expanded, tracker, ...otherProps } = props;
-    return (
-      <MuiAccordionSummary
-        expandIcon={
-          isExpanded && expanded !== "panel3" ? (
-            <KeyboardDoubleArrowRightOutlinedIcon sx={{ fontSize: "0.9rem" }} />
-          ) : (
-            <KeyboardDoubleArrowLeftOutlinedIcon sx={{ fontSize: "0.9rem" }} />
-          )
-        }
-        {...otherProps}
-      />
-    );
-  }
-)(() => ({
-  // backgroundColor: "rgba(255, 255, 255, .05)",
-  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
-    transform: "rotate(90deg)",
-  },
-  "& .MuiAccordionSummary-content": {
-    // marginLeft: "0.5%",
-    display: "flex",
-    gap: "1.5%",
-  },
-}));
-
-const AccordionDetails = styled(MuiAccordionDetails)(() => ({
-  padding: "0.2%",
-  borderTop: "1px solid rgba(0, 0, 0, .125)",
-  height: "calc(100vh - 320px)",
-  backgroundColor: "white",
-  overflow: "auto",
-}));
-
-// Want to eventually change this depending on if a generation has already occured or not
-const Container = styled(Grid)`
-  width: 46%;
-  padding: 0.3%;
-  // background-color: #f8f8ff;
-  background-color: white;
-  border-radius: 4px;
-  border: 1px solid #006d4b;
-  height: calc(100vh - 100px);
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-`;
-
-const SubContainer = styled(Grid)`
-  width: 100%;
-  // height: 96%;
-`;
-
-const ButtonContainer = styled(Grid)`
-  // margin: auto;
-  display: flex;
-  align-items: center;
-  marting-top: 20%;
-`;
-
-const GenerateButton = styled(UnSelectedButton)`
-  width: 55%;
-  background-color: #bacbba;
-  color: white;
-  font-size: 0.95rem;
-  letter-spacing: 1px;
-
-  &:hover {
-    background-color: #a5b4a5;
-    color: white;
-  }
-`;
-
 export default function GenerationSetup() {
+  // Contexts
   const { state } = useGenerationSetupContext();
+  const { generateCoverLetter, getJobTitle, getCompanyName, getJobMatchScore } =
+    useCoverLetterResultsContext();
 
+  // State from Context
   const {
     jobPostingInput,
     uploadedResumeFile,
     freeTextPersonalDetails,
     additionalDetails,
   } = state;
-  const { generateCoverLetter, getJobTitle, getCompanyName, getJobMatchScore } =
-    useCoverLetterResultsContext();
 
+  // Component State
   const [expanded, setExpanded] = React.useState<string | false>("panel1");
   const [previousPanel, setPreviousPanel] = React.useState<string | false>(
     "panel1"
   );
-
   const [jobPostingLastSubmitted, setJobPostingLastSubmitted] =
     useState<string>("");
-  const isDifferentJobPostingSinceLastSubmission = () => {
-    if (jobPostingInput === jobPostingLastSubmitted) {
-      return false;
-    } else {
-      return true;
-    }
-  };
-
   const [resumeLastUploaded, setResumeLastUploaded] = useState<any>({});
-  const isDifferentResumeSinceLastSubmission = () => {
-    if (jobPostingInput === jobPostingLastSubmitted) {
-      return false;
-    } else {
-      return true;
-    }
-  };
 
+  // Helper functions
+  const isDifferentJobPostingSinceLastSubmission = () =>
+    jobPostingInput !== jobPostingLastSubmitted;
+  const isDifferentResumeSinceLastSubmission = () =>
+    jobPostingInput !== jobPostingLastSubmitted;
+
+  // Upload handlers
   const handleJobPostingUpload = async () => {
-    if (isDifferentJobPostingSinceLastSubmission) {
+    if (isDifferentJobPostingSinceLastSubmission()) {
       try {
-        await uploadJobPosting(jobPostingInput);
+        // await uploadJobPosting(jobPostingInput);
+        setJobPostingLastSubmitted(jobPostingInput);
       } catch (err) {
         console.error(err);
         // Handle error, for example by showing an error message in the UI
       }
-      setJobPostingLastSubmitted(jobPostingInput);
     }
   };
 
   const handleResumeUpload = async () => {
-    if (isDifferentResumeSinceLastSubmission) {
+    if (isDifferentResumeSinceLastSubmission()) {
       try {
-        await uploadResume(uploadedResumeFile);
+        // await uploadResume(uploadedResumeFile);
+        setResumeLastUploaded(uploadedResumeFile);
       } catch (err) {
         console.error(err);
         // Handle error, for example by showing an error message in the UI
       }
-      setResumeLastUploaded(uploadedResumeFile);
     }
   };
 
+  // Cover Letter handler
   const handleGenerateCoverLetter = async () => {
     try {
       await getJobTitle(jobPostingInput);
-    } catch (err) {
-      console.log("error getting job title");
-      console.error(err);
-    }
-
-    try {
       await getCompanyName(jobPostingInput);
-    } catch (err) {
-      console.log("error getting job title");
-      console.error(err);
-    }
-
-    try {
       await getJobMatchScore(jobPostingInput);
-    } catch (err) {
-      console.log("error getting job title");
-      console.error(err);
-    }
-
-    try {
       await generateCoverLetter(
         jobPostingInput,
         uploadedResumeFile,
@@ -220,6 +100,7 @@ export default function GenerationSetup() {
     }
   };
 
+  // Panel change handler
   const handleChange =
     (panel: string, nextPanel: string | false, tracker: string) =>
     (event: React.SyntheticEvent, isExpanded: boolean) => {
@@ -242,19 +123,18 @@ export default function GenerationSetup() {
       }
     };
 
+  // Upload handlers
   useEffect(() => {
     if (previousPanel === "panel1" && expanded !== "panel1") {
       handleJobPostingUpload();
     } else if (previousPanel === "panel2" && expanded !== "panel2") {
-      // handleResumeUpload();
+      handleResumeUpload();
     } else {
       console.log("saveCurrentAdditionalDetails()");
       // saveCurrentAdditionalDetails()
     }
     setPreviousPanel(expanded);
   }, [expanded]);
-
-  console.log("resume", uploadedResumeFile);
 
   return (
     <Container>
@@ -270,29 +150,17 @@ export default function GenerationSetup() {
           )}
         >
           <AccordionSummary
-            aria-controls="panel1d-content"
-            id="panel1d-header"
             isExpanded={expanded === "panel1"}
             expanded="panel1"
             tracker={`1-${expanded === "panel1"}`}
           >
             {jobPostingInput === "" ? (
-              <CheckCircleOutlineIcon
-                style={{
-                  color: "#E9E9E9",
-                  opacity: 0.5,
-                }}
-              />
+              <CheckboxIconInComplete />
             ) : (
-              <CheckCircleOutlineIcon
-                style={{
-                  color: "lightgreen",
-                  opacity: 1,
-                }}
-              />
+              <CheckboxIconComplete />
             )}
 
-            <Typography>Job Posting</Typography>
+            <TypographyColored>Job Posting</TypographyColored>
           </AccordionSummary>
 
           <AccordionDetails>
@@ -310,28 +178,18 @@ export default function GenerationSetup() {
           )}
         >
           <AccordionSummary
-            aria-controls="panel2d-content"
-            id="panel2d-header"
             isExpanded={expanded === "panel2"}
             expanded="panel2"
             tracker={`2-${expanded === "panel2"}`}
           >
             {uploadedResumeFile === null && freeTextPersonalDetails === "" ? (
-              <CheckCircleOutlineIcon
-                style={{
-                  color: "#E9E9E9",
-                  opacity: 0.5,
-                }}
-              />
+              <CheckboxIconInComplete />
             ) : (
-              <CheckCircleOutlineIcon
-                style={{
-                  color: "lightgreen",
-                  opacity: 1,
-                }}
-              />
+              <CheckboxIconComplete />
             )}
-            <Typography>Résumé Upload / Personal Details</Typography>
+            <TypographyColored>
+              Résumé Upload / Personal Details
+            </TypographyColored>
           </AccordionSummary>
           <AccordionDetails>
             <PersonalDetails />
@@ -344,30 +202,18 @@ export default function GenerationSetup() {
           onChange={handleChange("panel3", false, `3-${expanded === "panel3"}`)}
         >
           <AccordionSummary
-            aria-controls="panel3d-content"
-            id="panel3d-header"
             isExpanded={expanded === "panel3"}
             expanded="panel3"
             tracker={`3-${expanded === "panel3"}`}
           >
             {jobPostingInput === "" ||
             (uploadedResumeFile === null && freeTextPersonalDetails === "") ? (
-              <CheckCircleOutlineIcon
-                style={{
-                  color: "#E9E9E9",
-                  opacity: 0.5,
-                }}
-              />
+              <CheckboxIconInComplete />
             ) : (
-              <CheckCircleOutlineIcon
-                style={{
-                  color: "lightgreen",
-                  opacity: 1,
-                }}
-              />
+              <CheckboxIconComplete />
             )}
 
-            <Typography>Additional Details (optional)</Typography>
+            <TypographyColored>Additional Details (optional)</TypographyColored>
           </AccordionSummary>
           <AccordionDetails>
             <AdditionalDetails />
