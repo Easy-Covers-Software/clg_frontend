03c0d98c32f414754067a47f16c8d151869f2383
David Silveira <daviewave@gmail.com>
2023-07-26 01:20:12 -0400

    added post delete functionality to enhance user experience

--- components/SavedCoverLetters/components/SavedDisplay/components/SavedLettersList.tsx
+++ components/SavedCoverLetters/components/SavedDisplay/components/SavedLettersList.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useEffect, useState } from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
@@ -11,7 +11,7 @@ import { useAuth } from "@/context/AuthContext";
 import { CircularProgress } from "@mui/material";
 
 export default function SavedLettersList() {
-  const { state, dispatch, deleteSavedCoverLetter } =
+  const { state, dispatch, deleteSavedCoverLetter, resetSelected } =
     useSavedCoverLettersContext();
   const {
     savedCoverLetters,
@@ -19,12 +19,17 @@ export default function SavedLettersList() {
     updateSavedCoverLettersList,
   } = state;
 
-  const { updateSnackbar } = useAuth();
+  const {
+    state: authState,
+    updateSnackbar,
+    openAlertDialogConfirm,
+  } = useAuth();
+  const { didConfirmAlert } = authState;
 
   console.log("savedCoverLetters", selectedCoverLetter);
   console.log("state", state);
 
-  const [selected, setSelected] = React.useState<number | null>(null);
+  const [selected, setSelected] = useState<number | null>(null);
 
   const handleToggle = (value: number) => () => {
     if (selected !== value) {
@@ -38,11 +43,13 @@ export default function SavedLettersList() {
     }
   };
 
-  const hamdleDelete = async () => {
+  const confirmDelete = async () => {
     const response = await deleteSavedCoverLetter();
+    console.log("attempted delete", response);
 
     if (response === "success") {
       updateSnackbar(true, "success", "Cover Letter Deleted Successfully");
+      resetSelected();
       dispatch({
         type: "SET_UPDATE_SAVED_COVER_LETTERS_LIST",
         payload: !updateSavedCoverLettersList,
@@ -50,6 +57,20 @@ export default function SavedLettersList() {
     } else {
       updateSnackbar(true, "error", "Error Deleting Cover Letter");
     }
+  };
+
+  useEffect(() => {
+    if (didConfirmAlert !== null && didConfirmAlert) {
+      confirmDelete();
+    }
+  }, [didConfirmAlert]);
+
+  const hamdleDelete = async () => {
+    openAlertDialogConfirm(
+      true,
+      "Delete Cover Letter",
+      "Are you sure you want to delete this cover letter?"
+    );
   };
 
   return (
