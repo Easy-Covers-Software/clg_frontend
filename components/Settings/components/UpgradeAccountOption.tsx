463ba4eb3930e97d760cccb4a56a55c2fc5dea9e
David Silveira <daviewave@gmail.com>
2023-07-27 03:06:20 -0400

    reduced code

--- components/Settings/components/UpgradeAccountOption.tsx
+++ components/Settings/components/UpgradeAccountOption.tsx
@@ -28,53 +28,38 @@ export default function UpgradeAccountOption({
       <PackageNameContainer>
         <Typography>{packages.name}</Typography>
 
-        {packages.name === "Dynamic Drafter" ||
-        packages.name === "Intro Drafter" ? (
-          <PricingButtonContainer>
-            <ButtonWithLabel>
-              <Typography fontSize={"0.72rem"}>GPT-3.5 Turbo</Typography>
-              <PackageNameButtonDouble
-                onClick={() => {
-                  handlePackageSelection(packages.price);
-                }}
-              >
-                {packages.price}
-              </PackageNameButtonDouble>
-            </ButtonWithLabel>
+        <PricingButtonContainer>
+          <ButtonWithLabel>
+            <Typography fontSize={"0.72rem"}>GPT-3.5 Turbo</Typography>
+            <PackageNameButtonDouble
+              onClick={() => {
+                handlePackageSelection(packages.price);
+              }}
+            >
+              {packages.price}
+            </PackageNameButtonDouble>
+          </ButtonWithLabel>
 
-            <ButtonWithLabel>
-              <Typography fontSize={"0.72rem"}>GPT-4</Typography>
-              <PackageNameButtonDouble
-                onClick={() => {
-                  handlePackageSelection(packages.price_gpt4);
-                }}
-              >
-                {packages.price_gpt4}
-              </PackageNameButtonDouble>
-            </ButtonWithLabel>
-          </PricingButtonContainer>
-        ) : (
-          <PackageNameButtonSingle
-            onClick={() => {
-              handlePackageSelection(packages.price);
-            }}
-          >
-            {packages.price}
-          </PackageNameButtonSingle>
-        )}
+          <ButtonWithLabel>
+            <Typography fontSize={"0.72rem"}>GPT-4</Typography>
+            <PackageNameButtonDouble
+              onClick={() => {
+                handlePackageSelection(packages.price_gpt4);
+              }}
+            >
+              {packages.price_gpt4}
+            </PackageNameButtonDouble>
+          </ButtonWithLabel>
+        </PricingButtonContainer>
       </PackageNameContainer>
 
       <PackageDetailsContainer>
         <FeatureList>
-          <ListItem>
-            <Typography fontSize={"0.85rem"}>{packages.features[0]}</Typography>
-          </ListItem>
-          <ListItem>
-            <Typography fontSize={"0.85rem"}>{packages.features[1]}</Typography>
-          </ListItem>
-          <ListItem>
-            <Typography fontSize={"0.85rem"}>{packages.features[2]}</Typography>
-          </ListItem>
+          {packages.features.map((feature) => (
+            <ListItem>
+              <Typography fontSize={"0.85rem"}>{feature}</Typography>
+            </ListItem>
+          ))}
         </FeatureList>
       </PackageDetailsContainer>
     </Container>
