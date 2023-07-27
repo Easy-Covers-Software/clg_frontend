1cfb4382008a3c962664004994b92dacc12bf948
David Silveira <daviewave@gmail.com>
2023-07-27 03:05:57 -0400

    updated settings / packages -- for now, could definietly look nicer

--- components/Settings/SettingsDialog.tsx
+++ components/Settings/SettingsDialog.tsx
@@ -13,36 +13,42 @@ import Paypal from "./components/Paypal";
 
 let packages = [
   {
-    name: "Cover Connoisseur",
-    features: [
-      "GPT-4 Access",
-      "Unlimited Cover Letters",
-      "Unlimited Adjustments",
-    ],
-    price: "$19.99/month",
-    price_gpt4: "NA",
+    name: "Intro Drafter",
+    features: ["5 Unique Cover Letters", "10 Adjustments", "GPT-4 or GPT-3.5"],
+    price: "$0.99",
+    price_gpt4: "$1.99",
+  },
+  {
+    name: "Dynamic Drafter",
+    features: ["10 unique cover letters", "20 Adjustments", "GPT-4 or GPT-3.5"],
+    price: "$1.99",
+    price_gpt4: "$2.99",
+  },
+  {
+    name: "3rd level",
+    features: ["20 unique cover letters", "40 Adjustments", "GPT-4 or GPT-3.5"],
+    price: "$2.99",
+    price_gpt4: "$5.99",
   },
   {
     name: "Letter Luminary",
     features: [
-      "GPT-3.5 Access",
-      "Unlimited Cover Letters",
-      "Unlimited Adjustments",
+      "75 unique cover letters",
+      "150 Adjustments",
+      "GPT-4 or GPT-3.5",
     ],
-    price: "$12.99/month",
-    price_gpt4: "NA",
+    price: "$4.99",
+    price_gpt4: "$9.99",
   },
   {
-    name: "Dynamic Drafter",
-    features: ["11 unique cover letters", "18 Adjustments", "GPT-4 or GPT-3.5"],
-    price: "$3.99",
-    price_gpt4: "$4.99",
-  },
-  {
-    name: "Intro Drafter",
-    features: ["2 Unique Cover Letters", "5 Adjustments", "GPT-4 or GPT-3.5"],
-    price: "$0.99",
-    price_gpt4: "$1.99",
+    name: "Cover Connoisseur",
+    features: [
+      "200 unique cover letters",
+      "400 Adjustments",
+      "GPT-4 or GPT-3.5",
+    ],
+    price: "$9.99",
+    price_gpt4: "$18.99",
   },
 ];
 
@@ -92,69 +98,15 @@ export default function SettingsDialog() {
       <FullLogo src="/easy-covers-full.svg" alt="Description of Image" />
 
       <DialogContentContainer>
-        <UpgradeAccountOption
-          packages={packages[0]}
-          setSelectedPackagePrice={setSelectedPackagePrice}
-        />
-        <UpgradeAccountOption
-          packages={packages[1]}
-          setSelectedPackagePrice={setSelectedPackagePrice}
-        />
-        <UpgradeAccountOption
-          packages={packages[2]}
-          setSelectedPackagePrice={setSelectedPackagePrice}
-        />
-        <UpgradeAccountOption
-          packages={packages[3]}
-          setSelectedPackagePrice={setSelectedPackagePrice}
-        />
+        {packages.map((packageItem) => (
+          <UpgradeAccountOption
+            packages={packageItem}
+            setSelectedPackagePrice={setSelectedPackagePrice}
+          />
+        ))}
 
         {hasSelectedPricingOption && (
-          <>
-            <Paypal selectedPackagePrice={selectedPackagePrice} />
-
-            {/* <PayPalButtons
-              style={{
-                layout: "horizontal",
-                height: 45,
-              }}
-              createOrder={(data, actions) => {
-                return actions.order.create({
-                  purchase_units: [
-                    {
-                      amount: {
-                        value: extractPrice(selectedPackagePrice),
-                      },
-                    },
-                  ],
-                });
-              }}
-              onCancel={() => (
-                <SnackbarAlert
-                  type="warning"
-                  message="Payment Cancelled"
-                  open={true}
-                />
-              )}
-              onError={(err) => {
-                <SnackbarAlert
-                  type="error"
-                  message="Error during transaction, please try again."
-                  open={true}
-                />;
-              }}
-              onApprove={(data, actions) => {
-                toggleSettingsIsOpen();
-                return actions.order.capture().then(function (details) {
-                  <SnackbarAlert
-                    type="success"
-                    message="Success! Upgrade Complete!"
-                    open={true}
-                  />;
-                });
-              }}
-            /> */}
-          </>
+          <Paypal selectedPackagePrice={selectedPackagePrice} />
         )}
       </DialogContentContainer>
     </Dialog>
