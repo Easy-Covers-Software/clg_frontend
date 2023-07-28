93316ca2ddf1264423466749b59e8c293c21ed1f
David Silveira <daviewave@gmail.com>
2023-07-28 18:52:01 -0400

    removed unnecessary functions

--- components/SavedCoverLetters/components/Edit/components/CoverLetter/components/DownloadOptionsMenu.tsx
+++ components/SavedCoverLetters/components/Edit/components/CoverLetter/components/DownloadOptionsMenu.tsx
@@ -1,19 +1,6 @@
-import * as React from "react";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
+import React from "react";
 
-import IconButton from "@mui/material/IconButton";
-import DownloadForOfflineOutlinedIcon from "@mui/icons-material/DownloadForOfflineOutlined";
-import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
-import PictureAsPdfOutlinedIcon from "@mui/icons-material/PictureAsPdfOutlined";
-import PostAddOutlinedIcon from "@mui/icons-material/PostAddOutlined";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
-import { Tooltip } from "@mui/material";
-
-import { useSavedCoverLettersContext } from "@/context/SavedCoverLettersContext";
-import { useAuth } from "@/context/AuthContext";
 
 import SaveNameInput from "./SaveNameInput";
 import DownloadDropdown from "./DownloadDropdown";
@@ -21,37 +8,6 @@ import { DownloadOptionsMenuStyledComponents } from "../CoverLetter.styles";
 const { Container } = DownloadOptionsMenuStyledComponents;
 
 export default function DownloadOptionsMenu() {
-  const { saveCoverLetterResults, generatePDF, generateDOCX } =
-    useSavedCoverLettersContext();
-  const { updateSnackbar } = useAuth();
-
-  const [downloadMenuAnchor, setDownloadMenuAnchor] =
-    React.useState<null | HTMLElement>(null);
-
-  const openDownload = Boolean(downloadMenuAnchor);
-
-  const handleDownload = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setDownloadMenuAnchor(event.currentTarget);
-  };
-  const handleDownloadPDF = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setDownloadMenuAnchor(event.currentTarget);
-  };
-  const handleDownloadDOCX = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setDownloadMenuAnchor(event.currentTarget);
-  };
-  const handleCloseDownload = () => {
-    setDownloadMenuAnchor(null);
-  };
-
-  // const handleSave = async () => {
-  //   const response = await saveCoverLetterResults();
-  //   if (response.status === 200) {
-  //     updateSnackbar(true, "success", "Cover Letter Saved Successfully");
-  //   } else {
-  //     updateSnackbar(true, "error", `Error saving cover letter: ${response}`);
-  //   }
-  // };
-
   return (
     <Container>
       <SaveNameInput />
