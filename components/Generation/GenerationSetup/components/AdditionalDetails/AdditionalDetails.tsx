2b69b0260cfb6aa590615393a9262f475f360de5
David Silveira <daviewave@gmail.com>
2023-07-29 03:35:14 -0400

    removed commented code

--- components/Generation/GenerationSetup/components/AdditionalDetails/AdditionalDetails.tsx
+++ components/Generation/GenerationSetup/components/AdditionalDetails/AdditionalDetails.tsx
@@ -7,13 +7,6 @@ import { Container } from "./AdditionalDetails.styles";
 export default function AdditionalDetails() {
   return (
     <Container>
-      {/* <Grid m={"auto"}>
-        <Typography variant="additionalDetailsInfo">
-          If you uploaded a resume and are confident with it, this generally
-          isn't needed.
-        </Typography>
-      </Grid> */}
-
       <SimpleInput id={"simpleInput1"} />
 
       <SimpleInput id={"simpleInput2"} />
