3c334876eaa15541bb64213e36f948a799571e76
David Silveira <daviewave@gmail.com>
2023-08-03 20:49:12 -0400

    fix react import error

--- components/Generation/GenerationSetup/GenerationSetup.tsx
+++ components/Generation/GenerationSetup/GenerationSetup.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 // React-related imports
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import { Typography } from "@mui/material";
 
