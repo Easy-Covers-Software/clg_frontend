4a3b91a29210cfb23fe871f47d884ecf4b756b56
David Silveira <daviewave@gmail.com>
2023-08-17 03:20:57 -0400

    made packages display horizonally if in full screen and column if mobile

--- components/Settings/components/UpgradeAccountOption.tsx
+++ components/Settings/components/UpgradeAccountOption.tsx
@@ -18,6 +18,7 @@ const {
 import { useMediaQuery } from "@mui/material";
 
 export default function UpgradeAccountOption({
+  key,
   packages,
   setSelectedPackagePrice,
 }) {
@@ -84,8 +85,17 @@ export default function UpgradeAccountOption({
 
         <PackageDetailsContainer>
           <FeatureList>
-            {packages.features.map((feature, i) => (
-              <Typography key={i} className="package-features">{feature}</Typography>
+            {packages.features.map((feature, i: number) => (
+              <>
+                <Typography key={i} className="package-features">
+                  {feature}
+                </Typography>
+                {i === 0 && (
+                  <Typography className="package-features">
+                    &nbsp;&&&nbsp;
+                  </Typography>
+                )}
+              </>
             ))}
           </FeatureList>
         </PackageDetailsContainer>
