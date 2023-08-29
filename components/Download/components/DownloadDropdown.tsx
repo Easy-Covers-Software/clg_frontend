210364f8a19340d3bff00d02f19cf10864fc851b
David Silveira <davidsilveira.3.djs@gmail.com>
2023-08-29 17:24:48 -0400

    added seperate download dropdown components

---
+++ components/Download/components/DownloadDropdown.tsx
@@ -0,0 +1,161 @@
+import { useState, MouseEvent, FC } from 'react';
+import {
+  Menu,
+  MenuItem,
+  IconButton,
+  Tooltip,
+  ListItemIcon,
+  ListItemText,
+  useMediaQuery,
+} from '@mui/material';
+
+import PictureAsPdfOutlinedIcon from '@mui/icons-material/PictureAsPdfOutlined';
+import PostAddOutlinedIcon from '@mui/icons-material/PostAddOutlined';
+
+import DownloadForOfflineOutlinedIcon from '@mui/icons-material/DownloadForOfflineOutlined';
+
+// import { DownloadProperties } from '../DownloadMenu.types';
+import { DownloadProps } from '@/Types/GenerationContext.types';
+import { Snackbar } from '@/Types/AuthContext.types';
+
+import { Helpers, DownloadMethods } from '@/Utils/utils';
+const { generatePDF, generateDOCX } = DownloadMethods;
+const { determineCoverLetterParts, determineCoverLetterHtml } = Helpers;
+
+import { CoverLetterData } from '@/Types/GenerationContext.types';
+
+interface Props {
+  coverLetterData: CoverLetterData;
+  downloadProps: DownloadProps;
+  snackbar: Snackbar;
+}
+
+const DownloadDropdown: FC<Props> = ({
+  coverLetterData,
+  downloadProps,
+  snackbar,
+}) => {
+  const isMobile = useMediaQuery('(max-width: 600px)');
+
+  const [downloadMenuAnchor, setDownloadMenuAnchor] =
+    useState<null | HTMLElement>(null);
+
+  const handleDownload = (event: MouseEvent<HTMLButtonElement>) => {
+    setDownloadMenuAnchor(event.currentTarget);
+    downloadProps.toggleIsDownloadDropdownOpen();
+  };
+
+  const handleCloseDownload = () => {
+    setDownloadMenuAnchor(null);
+    downloadProps.toggleIsDownloadDropdownOpen();
+  };
+
+  const handlePDFDownload = () => {
+    const pdf = generatePDF(
+      determineCoverLetterParts(coverLetterData),
+      coverLetterData.saveName
+    );
+
+    if (pdf) {
+      snackbar.updateSnackbar(
+        true,
+        'success',
+        'Successfully Exported Cover Letter as PDF'
+      );
+    } else {
+      snackbar.updateSnackbar(
+        true,
+        'error',
+        'Error Exporting Cover Letter as PDF'
+      );
+    }
+    handleCloseDownload();
+  };
+
+  const handleDOCXDownload = async () => {
+    const response = await generateDOCX(
+      determineCoverLetterHtml(coverLetterData),
+      coverLetterData.saveName
+    );
+
+    if (response) {
+      snackbar.updateSnackbar(
+        true,
+        'success',
+        `Success! Exported ${coverLetterData.saveName} as Docx.`
+      );
+    } else {
+      snackbar.updateSnackbar(
+        true,
+        'error',
+        `Error exporting ${coverLetterData.saveName} as Docx.`
+      );
+    }
+    handleCloseDownload();
+  };
+
+  const shouldDisable = (coverLetterData: CoverLetterData) => {
+    if (coverLetterData?.coverLetterHtml) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const disabled = shouldDisable(coverLetterData);
+
+  return (
+    <>
+      <Tooltip title='Download'>
+        <IconButton
+          onClick={handleDownload}
+          disableRipple
+          disableFocusRipple
+          style={{
+            margin: isMobile ? 'auto' : 'none',
+          }}
+          disabled={disabled}
+        >
+          <DownloadForOfflineOutlinedIcon />
+        </IconButton>
+      </Tooltip>
+
+      <Menu
+        id='basic-menu'
+        anchorEl={downloadMenuAnchor}
+        open={downloadProps?.isDownloadDropdownOpen}
+        onClose={handleCloseDownload}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        sx={{
+          marginTop: '1%',
+        }}
+      >
+        <MenuItem onClick={handlePDFDownload} disableRipple>
+          <ListItemIcon>
+            <PictureAsPdfOutlinedIcon />
+          </ListItemIcon>
+          <ListItemText> Download as PDF</ListItemText>
+        </MenuItem>
+
+        <MenuItem onClick={handleDOCXDownload} disableRipple>
+          <ListItemIcon>
+            <PostAddOutlinedIcon />
+          </ListItemIcon>
+          <ListItemText>Download as DOCX</ListItemText>
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default DownloadDropdown;
