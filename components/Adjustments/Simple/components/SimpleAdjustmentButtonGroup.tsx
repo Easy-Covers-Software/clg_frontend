3159c22d54913b82c01bf260ab4d2244498a0ee5
David Silveira <davidsilveira.3.djs@gmail.com>
2023-08-29 17:21:59 -0400

    added simple adjustments button group component

---
+++ components/Adjustments/Simple/components/SimpleAdjustmentButtonGroup.tsx
@@ -0,0 +1,66 @@
+import { FC } from 'react';
+import Divider from '@mui/material/Divider';
+import { Typography, IconButton } from '@mui/material';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import RemoveCircleOutlineOutlinedIcon from '@mui/icons-material/RemoveCircleOutlineOutlined';
+
+import { useGenerationContext } from '@/context/GenerationContext';
+import { useAuth } from '@/context/AuthContext';
+
+import { Helpers, AdjustmentsUtils } from '@/Utils/utils';
+const { isOriginalGenerationEdited } = Helpers;
+const { makeSimpleAdjustment } = AdjustmentsUtils;
+
+import { SimpleAdjustmentsButtonGroupStyledComponents } from '../SimpleAdjustments.styles';
+const { Container, ButtonContainer } =
+  SimpleAdjustmentsButtonGroupStyledComponents;
+
+/*
+  buttonLabel (this probably needs to be updated):
+    - "1" = "Years of relevant experience"
+    - "2" = "Relevant Awards / Skills / Certifications"
+    - "3" = "Projects / Publications / Languages"
+*/
+
+interface Props {
+  buttonLabel: string;
+  disabled: boolean;
+  handleDecreaseSimpleAdjustment: (buttonLabel: string) => Promise<void>;
+  handleIncreaseSimpleAdjustment: (buttonLabel: string) => void;
+}
+
+const SimpleAdjustmentButtonGroup: FC<Props> = ({
+  buttonLabel,
+  disabled,
+  handleDecreaseSimpleAdjustment,
+  handleIncreaseSimpleAdjustment,
+}) => {
+  return (
+    <Container>
+      <Typography className='simple-adjustment-label'>{buttonLabel}</Typography>
+      <ButtonContainer>
+        <IconButton
+          disabled={disabled}
+          onClick={() => {
+            handleDecreaseSimpleAdjustment(buttonLabel);
+          }}
+        >
+          <RemoveCircleOutlineOutlinedIcon />
+        </IconButton>
+
+        <Divider orientation='vertical' />
+
+        <IconButton
+          disabled={disabled}
+          onClick={() => {
+            handleIncreaseSimpleAdjustment(buttonLabel);
+          }}
+        >
+          <AddCircleOutlineOutlinedIcon />
+        </IconButton>
+      </ButtonContainer>
+    </Container>
+  );
+};
+
+export default SimpleAdjustmentButtonGroup;
