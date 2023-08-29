490fa9c8cd841ab3d22cd0e30d8393632fe7a6b4
David Silveira <davidsilveira.3.djs@gmail.com>
2023-08-29 17:23:03 -0400

    moved custom adjustment with the advanced adjustments components folde

---
+++ components/Adjustments/Advanced/components/CustomAdjustment.tsx
@@ -0,0 +1,113 @@
+import { useState, FC } from 'react';
+
+import Typography from '@mui/material/Typography';
+import { useAuth } from '@/context/AuthContext';
+
+import { Helpers, CoverLetterApiMethods } from '@/Utils/utils';
+const { makeAdjustment } = CoverLetterApiMethods;
+const { determineCoverLetterHtml } = Helpers;
+
+// import { CustomReQueryStyledComponents } from '../ReQueryOptions.styles';
+// const { CustomReQueryField, SubmitButton } = CustomReQueryStyledComponents;
+import { CustomAdjustmentStyledComponents } from '../AdvancedAdjustments.styles';
+const { Container, SubContainer, CustomReQueryField, SubmitButton } =
+  CustomAdjustmentStyledComponents;
+import { useMediaQuery } from '@mui/material';
+
+import {
+  CoverLetterData,
+  CustomAdjustmentProps,
+  AdjustmentSection,
+} from '@/Types/GenerationContext.types';
+
+interface Props {
+  coverLetterData: CoverLetterData;
+  customAdjustmentProps: CustomAdjustmentProps;
+}
+
+const CustomAdjustment: FC<Props> = ({
+  coverLetterData,
+  customAdjustmentProps,
+}) => {
+  const { state } = useAuth();
+  const { snackbar } = state;
+
+  const [placeholder, setPlaceholder] = useState(
+    'Anything you want to change about the cover letter...'
+  );
+
+  const handleChange = (e) => {
+    customAdjustmentProps?.updateCustomAdjustment(e.target.value);
+  };
+
+  const handleFocus = () => {
+    setPlaceholder('');
+  };
+
+  const handleBlur = () => {
+    if (customAdjustmentProps?.customAdjustment === '') {
+      setPlaceholder('Anything you want to change about the cover letter...');
+    }
+  };
+
+  const handleCustomAdjustment = async (): Promise<void> => {
+    coverLetterData?.toggleLoadingCoverLetter();
+
+    const response = await makeAdjustment(
+      'custom',
+      '',
+      customAdjustmentProps?.customAdjustment,
+      determineCoverLetterHtml(coverLetterData)
+    );
+
+    if (response.data) {
+      coverLetterData?.updateCoverLetterParts(response.data.cover_letter);
+      coverLetterData?.toggleLoadingCoverLetter();
+      snackbar.updateSnackbar(true, 'success', 'Adjustment made successfully.');
+    } else {
+      coverLetterData?.toggleLoadingCoverLetter();
+      snackbar.updateSnackbar(
+        true,
+        'error',
+        'An error occured while making adjustment. Please try again.'
+      );
+    }
+  };
+
+  const shouldDisable = (coverLetterData: CoverLetterData) => {
+    if (
+      coverLetterData?.loadingCoverLetter ||
+      coverLetterData?.coverLetterHtml === '' ||
+      customAdjustmentProps?.customAdjustment === ''
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const disable = shouldDisable(coverLetterData);
+
+  return (
+    <Container>
+      <SubContainer>
+        <Typography className='custom-adjustment-heading'>
+          Custom Adjustment
+        </Typography>
+        <CustomReQueryField
+          placeholder={placeholder}
+          value={customAdjustmentProps?.customAdjustment}
+          onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
+      </SubContainer>
+
+      <SubmitButton onClick={handleCustomAdjustment} disabled={disable}>
+        REGENERATE
+      </SubmitButton>
+    </Container>
+  );
+};
+
+export default CustomAdjustment;
