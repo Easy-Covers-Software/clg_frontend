34428a5fc9ec78311fe107be71f7085bd6303c70
David Silveira <davidsilveira.3.djs@gmail.com>
2023-08-29 17:27:14 -0400

    added simple input component for new simple adjustment component

--- components/Generation/GenerationSetup/components/AdditionalDetails/components/SimpleInput.tsx
+++ components/Generation/GenerationSetup/components/AdditionalDetails/components/SimpleInput.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import IconButton from "@mui/material/IconButton";
-import InputAdornment from "@mui/material/InputAdornment";
-import HighlightOffIcon from "@mui/icons-material/HighlightOff";
-import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
-import Tooltip from "@mui/material/Tooltip";
-import { useGenerationContext } from "@/context/GenerationContext";
-import { SimpleInputStyledComponents } from "../AdditionalDetails.styles";
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import Tooltip from '@mui/material/Tooltip';
+import { useGenerationContext } from '@/context/GenerationContext';
+import { SimpleInputStyledComponents } from '../AdditionalDetails.styles';
 const { Container, InputField } = SimpleInputStyledComponents;
 
 interface SimpleInputProps {
@@ -15,68 +15,58 @@ interface SimpleInputProps {
 
 export default function SimpleInput(props: SimpleInputProps) {
   const { id } = props;
-  const { state, dispatch } = useGenerationContext();
+  const { state } = useGenerationContext();
   const { additionalDetails } = state;
 
-  const [inputValue, setInputValue] = useState("");
-
   const clearInput = () => {
-    setInputValue("");
-  };
-
-  const updateCurrentInputState = (currentInput) => {
-    for (const [key, inputValue] of Object.entries(additionalDetails)) {
-      if (key === id) {
-        setInputValue(currentInput);
-        dispatch({
-          type: "SET_ADDITIONAL_DETAILS",
-          payload: { [key]: currentInput },
-        });
-      }
-    }
+    additionalDetails.updateSimpleInput(id, '');
   };
 
   const getPlaceholderText = (id: string) => {
     switch (id) {
-      case "simpleInput1":
-        return "Years of relevant experience...";
-      case "simpleInput2":
-        return "Relevant Awards / Skills / Certifications...";
+      case '1':
+        return 'Years of relevant experience (2019-present)';
+      case '2':
+        return 'Relevant Awards / Skills / Certifications...';
       default:
-        return "Relevant Projects / Publications...";
+        return 'Relevant Projects / Publications...';
     }
   };
 
   const getTooltipTitle = (id: string) => {
     switch (id) {
-      case "simpleInput1":
-        return "The years you worked most recently worked in the field of the job you are applying for";
-      case "simpleInput2":
-        return "Any awards, skills, or certifications that you might have left out of your résumé but are applicable to the job you are applying for.";
+      case 'simpleInput1':
+        return 'The years you worked most recently worked in the field of the job you are applying for';
+      case 'simpleInput2':
+        return 'Any awards, skills, or certifications that you might have left out of your résumé but are applicable to the job you are applying for.';
       default:
-        return "Any projects or publications that you might have left out of your résumé but are applicable to the job you are applying for.";
+        return 'Any projects or publications that you might have left out of your résumé but are applicable to the job you are applying for.';
     }
+  };
+
+  const getValue = (id: string) => {
+    return Object.values(additionalDetails)[parseInt(id) - 1];
   };
 
   return (
     <Container>
       <InputField
-        id="email-input"
-        variant="outlined"
+        id='email-input'
+        variant='outlined'
         placeholder={getPlaceholderText(id)}
-        size="small"
-        value={inputValue}
+        size='small'
+        value={getValue(id)}
         onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-          updateCurrentInputState(event.target.value);
+          additionalDetails.updateSimpleInput(id, event.target.value);
         }}
         InputProps={{
           endAdornment: (
-            <InputAdornment position="end">
+            <InputAdornment position='end'>
               <IconButton
-                aria-label="toggle password visibility"
+                aria-label='toggle password visibility'
                 onClick={clearInput}
-                edge="end"
-                sx={{ opacity: "30%" }}
+                edge='end'
+                sx={{ opacity: '30%' }}
               >
                 <HighlightOffIcon />
               </IconButton>
@@ -84,8 +74,8 @@ export default function SimpleInput(props: SimpleInputProps) {
           ),
         }}
       />
-      <Tooltip title={getTooltipTitle(id)} placement="top">
-        <InfoOutlinedIcon fontSize="medium" sx={{ opacity: "40%" }} />
+      <Tooltip title={getTooltipTitle(id)} placement='top'>
+        <InfoOutlinedIcon fontSize='medium' sx={{ opacity: '40%' }} />
       </Tooltip>
     </Container>
   );
