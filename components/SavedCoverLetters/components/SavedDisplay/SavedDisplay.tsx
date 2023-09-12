9679c7b6828967f9c6a433f055230e1b7e8f0aa0
David Silveira <davidsilveira.3.djs@gmail.com>
2023-09-12 15:27:18 -0400

    change double quote to single

--- components/SavedCoverLetters/components/SavedDisplay/SavedDisplay.tsx
+++ components/SavedCoverLetters/components/SavedDisplay/SavedDisplay.tsx
@@ -1,15 +1,15 @@
-import { Container, SubContainer } from "./SavedDisplay.styles";
+import { Container, SubContainer } from './SavedDisplay.styles';
 
-import SearchAndFilter from "./components/SearchAndFilter";
-import SavedLettersList from "./components/SavedLettersList";
+import SearchAndFilter from './components/SearchAndFilter';
+import SavedLettersList from './components/SavedLettersList';
 
-import { Typography } from "@mui/material";
+import { Typography } from '@mui/material';
 
 export default function SavedDisplay() {
   return (
     <Container>
       <SubContainer>
-        <Typography className="saved-header">Saved Cover Letters</Typography>
+        <Typography className='saved-header'>Saved Cover Letters</Typography>
         <SearchAndFilter />
         <SavedLettersList />
       </SubContainer>
