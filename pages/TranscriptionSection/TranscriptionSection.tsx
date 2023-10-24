45fad8f1987e36e941a289b8b38153d66331ab7e
David Silveira <davidsilveira.3.djs@gmail.com>
2023-10-24 00:43:25 -0400

    added transcription section

---
+++ pages/TranscriptionSection/TranscriptionSection.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+// External Libraries
+import { useEffect } from 'react';
+import Grid from '@mui/material/Unstable_Grid2/Grid2';
+import styled from '@emotion/styled';
+import { useMediaQuery } from '@mui/material';
+
+// Context
+import { useAuth } from '@/context/AuthContext';
+import TranscriptionContext from '@/context/TranscriptionContext';
+// import SavedCoverLettersContext from '@/context/SavedCoverLettersContext';
+
+// Components (global)
+import SnackbarAlert from '../../components/Global/components/SnackbarAlert';
+import AlertDialogConfirm from '../../components/Global/components/AlertDialogConfirm';
+
+// Components (local)
+import LoginDialog from '@/components/Login/LoginDialog';
+import SettingsDialog from '@/components/Settings/SettingsDialog';
+
+import SavedPhoneCalls from './components/SavedPhoneCalls/SavedPhoneCalls';
+import CoverLetterEdit from '../SavedProfilesSection/components/CoverLetterEdit/CoverLetterEdit';
+
+import TranscriptionBody from './components/TranscriptionBody/TranscriptionBody';
+
+// import SavedProfilesList from './components/SavedProfilesList/SavedProfilesList';
+
+const Container = styled(Grid)`
+  display: flex;
+  // justify-content: space-between;
+  justify-content: end;
+  width: 100%;
+`;
+
+export default function TranscriptionPage() {
+  const isMobile = useMediaQuery('(max-width: 600px)');
+  const { state } = useAuth();
+
+  const { trackers, dialogProps, snackbar, confirmDialog } = state;
+
+  useEffect(() => {
+    if (trackers.updatePage) {
+      trackers.updatePage('transcription');
+    }
+  }, [trackers.updatePage]);
+
+  return (
+    <Container>
+      {dialogProps.isLoginOpen ? <LoginDialog /> : null}
+      {/* {dialogProps.isSettingsOpen ? <SettingsDialog /> : null} */}
+      <TranscriptionContext>
+        {isMobile ? (
+          <>
+            {trackers.mobileModeSaved === 'choose' ? (
+              <SavedPhoneCalls />
+            ) : (
+              <TranscriptionBody />
+            )}
+          </>
+        ) : (
+          <>
+            <SavedPhoneCalls />
+            <TranscriptionBody />
+          </>
+        )}
+      </TranscriptionContext>
+
+      <SnackbarAlert
+        open={snackbar.open}
+        type={snackbar.type}
+        message={snackbar.message}
+      />
+
+      <AlertDialogConfirm
+        open={confirmDialog.open}
+        header={confirmDialog.header}
+        message={confirmDialog.message}
+        buttonText={confirmDialog.buttonText}
+      />
+    </Container>
+  );
+}
