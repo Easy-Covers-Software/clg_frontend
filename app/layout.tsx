472f5007645564dc3a8abf7b9d0351dcd1ebce6b
David Silveira <davidsilveira.3.djs@gmail.com>
2023-10-24 00:34:21 -0400

    format change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,16 @@
-import Provider from "@/components/Provider";
+import Provider from '@/components/Provider';
 
-import axios from "axios";
+import axios from 'axios';
 
 axios.defaults.withCredentials = true;
-import Document, { Html, Head, Main, NextScript } from "next/document";
 
 const RootLayout = ({ children }) => {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+          name='viewport'
+          content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'
         />
       </head>
 
