493570aba12f39d3c25febebf336c7f879bc1230
David Silveira <davidsilveira.3.djs@gmail.com>
2023-10-24 02:47:33 -0400

    added websocket logic for transcription and changed button style

--- pages/TranscriptionSection/components/SavedPhoneCalls/SavedPhoneCalls.tsx
+++ pages/TranscriptionSection/components/SavedPhoneCalls/SavedPhoneCalls.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Container } from './SavedPhoneCalls.styles';
 
+import styled from '@emotion/styled';
 // Context
 import { useAuth } from '@/context/AuthContext';
 import { useTranscriptionContext } from '@/context/TranscriptionContext';
@@ -9,8 +10,32 @@ import { useTranscriptionContext } from '@/context/TranscriptionContext';
 import SavedList from '@/components/SavedList/SavedList';
 import { UnSelectedButton, PrimaryButton } from '@/components/Global/Global';
 
+import ReconnectingWebSocket from 'reconnecting-websocket';
+
 import { TranscriptionMethods } from '@/Utils/utils';
 const { fetchPhoneCalls, performTranscription } = TranscriptionMethods;
+
+export const TranscribeButton = styled(UnSelectedButton)`
+  // height: 88%;
+  width: 98%;
+  margin: auto;
+
+  background-color: #bacbba;
+  color: white;
+  font-size: 0.95rem;
+  letter-spacing: 1px;
+  white-space: nowrap;
+
+  &:hover {
+    background-color: #a5b4a5;
+    color: white;
+  }
+  &:disabled {
+    background-color: #e9e9e9;
+    color: lightgray;
+    border: 1px solid lightgray;
+  }
+`;
 
 export default function SavedPhoneCalls() {
   const { state: authState } = useAuth();
@@ -18,7 +43,9 @@ export default function SavedPhoneCalls() {
 
   const { state, dispatch } = useTranscriptionContext();
   console.log(state);
-  const { phoneCallState } = state;
+  const { phoneCallState, transcriptionState } = state;
+
+  const [socket, setSocket] = useState<ReconnectingWebSocket | null>(null);
 
   // fetch saved phone calls
   const getSavedPhoneCalls = async (): Promise<void> => {
@@ -55,8 +82,18 @@ export default function SavedPhoneCalls() {
   };
 
   const handleTranscribe = async () => {
-    const response = await performTranscription(phoneCallState?.selected?.id);
+    // const response = await performTranscription(phoneCallState?.selected?.id);
+    const response = true;
     if (response) {
+      dispatch({
+        type: 'UPDATE_TRANSCRIPTION_LOADING',
+        payload: !transcriptionState?.loading,
+      });
+      dispatch({
+        type: 'SET_CURRENT_MODE',
+        payload: 'Notes',
+      });
+      handleWebSocketConnection();
       snackbar?.updateSnackbar(
         true,
         'success',
@@ -67,9 +104,48 @@ export default function SavedPhoneCalls() {
     }
   };
 
+  const handleWebSocketConnection = () => {
+    const wsOptions = {
+      WebSocket: WebSocket,
+      connectionTimeout: 1000,
+      maxRetries: 10,
+    };
+
+    const ws = new ReconnectingWebSocket(
+      'ws://127.0.0.1:8001/ws/transcription/status/',
+      [],
+      wsOptions
+    );
+
+    ws.addEventListener('message', (event) => {
+      console.log('Web socket');
+      const data = JSON.parse(event.data);
+      console.log(data);
+
+      if (data.transcription_status) {
+        dispatch({
+          type: 'UPDATE_TRANSCRIPTION_STATUS',
+          payload: data.transcription_status,
+        });
+      }
+      // Check for the 'completed' status and close the connection
+      if (data.call_status === 'completed') {
+        console.log('Closing the connection');
+        ws.close();
+      }
+    });
+
+    setSocket(ws);
+
+    return ws;
+  };
+
   useEffect(() => {
     getSavedPhoneCalls();
   }, []);
+
+  const isTranscribeDisabled =
+    !phoneCallState?.selected || transcriptionState?.loading;
 
   return (
     <Container>
@@ -82,7 +158,12 @@ export default function SavedPhoneCalls() {
         handleToggle={handleToggle}
         handleSearchChange={handleSearchChange}
       />
-      <PrimaryButton onClick={handleTranscribe}>Transcribe</PrimaryButton>
+      <TranscribeButton
+        disabled={isTranscribeDisabled}
+        onClick={handleTranscribe}
+      >
+        Transcribe
+      </TranscribeButton>
     </Container>
   );
 }
