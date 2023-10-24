1593585f65c4d1f9774fc6cacf628c69e6c6613f
David Silveira <davidsilveira.3.djs@gmail.com>
2023-10-24 00:33:42 -0400

    added transcription methods for the api calls for phone calls and transcription

--- Utils/utils.ts
+++ Utils/utils.ts
@@ -21,6 +21,12 @@ import {
   CoverLetterData,
 } from '@/Types/GenerationContext.types';
 
+import {
+  PhoneCall,
+  PhoneCallState,
+  InitateCallResponse,
+} from '@/Types/TranscriptionPage.types';
+
 const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
@@ -412,9 +418,11 @@ namespace CoverLetterApiMethods {
   ): Promise<APIResponse<CoverLetterGenerateApiResponse>> => {
     const url = `${API_BASE_URL}/generate/`;
 
+    console.log('additional details', additionalDetails);
+
     const data = {
       model: model,
-      additional_details: additionalDetails,
+      additional_details: JSON.stringify(additionalDetails),
     };
 
     const payload: FormData = Helpers.createPayload(data);
@@ -684,4 +692,112 @@ namespace DownloadMethods {
   };
 }
 
-export { Helpers, LoginApiMethods, CoverLetterApiMethods, DownloadMethods };
+namespace TranscriptionMethods {
+  //-- Get Phone Calls Associated with Users Phone Number --//
+  // TODO: need to add a type for the response
+  export const fetchPhoneCalls = async (): Promise<
+    APIResponse<PhoneCall[]>
+  > => {
+    const url = `${API_BASE}/phone_calls/get_phone_calls/`;
+
+    try {
+      const response = await axios.get<PhoneCall[]>(url, {
+        withCredentials: true,
+      });
+
+      return { data: response.data, error: null };
+    } catch (error) {
+      console.log('Error fetching phone calls', error);
+      return { data: null, error: error };
+    }
+  };
+
+  //-- Perform Transcription --//
+  export const performTranscription = async (
+    phoneCallId: string
+  ): Promise<APIResponse<any>> => {
+    const url = `${API_BASE}/transcription/perform_transcription/`;
+
+    const payload = Helpers.createPayload({ phone_call_id: phoneCallId });
+
+    try {
+      const response = await axios.post<any>(url, payload, {
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': Cookie.get('csrftoken'),
+        },
+      });
+
+      return { data: response.data, error: null };
+    } catch (error) {
+      console.log('Error performing transcription', error);
+      return { data: null, error: error };
+    }
+  };
+
+  //-- Fetch transcription from ID --//
+  export const fetchTranscription = async (
+    transcriptionId: string
+  ): Promise<APIResponse<any>> => {
+    const url = `${API_BASE}/transcription/${transcriptionId}/`;
+
+    try {
+      const response = await axios.get<any>(url, {
+        withCredentials: true,
+      });
+
+      return { data: response.data, error: null };
+    } catch (error) {
+      console.log('Error fetching transcription', error);
+      return { data: null, error: error };
+    }
+  };
+
+  export const initiatePhoneCall = async (
+    candidate_name: string,
+    candidate_number: string,
+    job_posting_id: string,
+    email: string | null,
+    linkedin_profile: string | null,
+    portfolio_website: string | null,
+    location: string | null
+  ): Promise<APIResponse<InitateCallResponse>> => {
+    const url = `${API_BASE}/phone_calls/initiate_call/`;
+
+    const data = {
+      candidate_name,
+      candidate_number,
+      job_posting_id,
+      email,
+      linkedin_profile,
+      portfolio_website,
+      location,
+    };
+
+    const payload = Helpers.createPayload(data);
+
+    try {
+      const response = await axios.post<InitateCallResponse>(url, payload, {
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': Cookie.get('csrftoken'),
+        },
+      });
+
+      return { data: response.data, error: null };
+    } catch (error) {
+      console.log('Error initiating phone call', error);
+      return { data: null, error: error };
+    }
+  };
+}
+
+export {
+  Helpers,
+  LoginApiMethods,
+  CoverLetterApiMethods,
+  DownloadMethods,
+  TranscriptionMethods,
+};
